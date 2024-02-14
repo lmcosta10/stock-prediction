528ca64779a274275a9d99945a459e3622fa93d6
lmcosta10 <letmc14@gmail.com>
2024-02-14 19:29:01 -0300

    style: comment indentation

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ class WindowGenerator():
     def __init__(self, input_width, label_width, shift,
                 train_df, val_df, test_df,
                 label_columns=None):
-    # Store the raw data.
+        # Store the raw data.
         self.train_df = train_df
         self.val_df = val_df
         self.test_df = test_df
